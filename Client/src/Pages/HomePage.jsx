967072a2cab53e0f9e7257a84eca7704c768beed
Sanjay-10 <99119329+Sanjay-10@users.noreply.github.com>
2025-01-10 20:10:01 -0500

    Extension Download link added

--- Client/src/Pages/HomePage.jsx
+++ Client/src/Pages/HomePage.jsx
@@ -27,7 +27,7 @@ const HomePage = () => {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="#"
+                href="https://chromewebstore.google.com/detail/skillmatch/nkibjapabhjcmcgihbkbmogggdaaofla"
                  target="_blank"
         rel="noopener noreferrer"
                 className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
